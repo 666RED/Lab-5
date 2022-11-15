b341880526e9a2417844ac6a981176a26116b9c7
666RED <90583566+666RED@users.noreply.github.com>
2022-11-15 17:38:06 +0800

    Add files via upload

---
+++ lab5/question3.java
@@ -0,0 +1,26 @@
+package com.hongchen.lab5;
+
+import java.util.Scanner;
+
+public class question3 {
+	public static void main(String[] arg) {
+				
+		Scanner scanner = new Scanner(System.in);
+
+		int num1 = (int)(Math.random() * 10);
+		int num2 = (int)(Math.random() * 10);
+		System.out.print("What is " + num1 + " + " + num2 + "?");
+		int answer = scanner.nextInt();
+		
+		while(answer != num1 + num2) {
+			if(answer == num1 + num2) {
+				break;
+			}else {
+				System.out.print("Wrong answer. Try again. ");
+				System.out.print("What is " + num1 + " + " + num2 + "?");
+				answer = scanner.nextInt();
+			}
+		}
+		System.out.println("You got it!");
+	}
+}
